92eb06f2c78470e3707564793f0b98d18beef511
POST2 <post2.prog2@enetcom.usf.tn>
2022-11-15 11:16:30 +0100

    addapplication

---
+++ application/app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -0,0 +1,45 @@
+package com.example.recyclerview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter  extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder>{
+    public WordListAdapter(LinkedList<String> mWordList) {
+        this.mWordList = mWordList;
+    }
+
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+        public WordViewHolder(@NonNull View itemView, WordListAdapter mAdapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = mAdapter;
+        }
+    }
+    private final LinkedList<String> mWordList;
+}
